3ecdb27e9035674aad5c54c3a0eafc2cb841d94c
Kofi Collins-Sibley <kcollinssibley@starry.com>
2019-07-07 19:50:19 -0400

    Add api & lib directories

--- pi_welcome/mbta.py
+++
@@ -1,9 +0,0 @@
-from pi_welcome import app
-
-MBTA_API = "https://api-v3.mbta.com"
-ROXBURY_ID = "70009"
-INBOUND_DIR = "1"
-
-@app.app.route('/api/mbta')
-def getMBTAPredition():
-    return "TODO(KCS): call MBTA lib function here"
