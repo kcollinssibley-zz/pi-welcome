8d4dff7f87bc327aed07cff894ef4d3dbedac12e
Kofi Collins-Sibley <kcollinssibley@starry.com>
2019-07-12 22:18:02 -0400

    Add sphinx auto documentation

---
+++ source/app.py
@@ -0,0 +1,20 @@
+.. pi-welcome documentation master file, created by
+   sphinx-quickstart on Fri Jul 12 22:16:23 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to pi-welcome's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
